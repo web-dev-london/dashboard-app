698b52f06fe2bfbf39a7009e4449d63aefcd45fe
Murod Akhmedov <murod.london@gmail.com>
2024-07-15 16:33:00 +0100

    Create dashboard component

--- src/components/ProgressCircle.tsx
+++ src/components/ProgressCircle.tsx
@@ -9,22 +9,20 @@ interface ProgressCircleProps {
 const ProgressCircle = ({ progress = '0.75', size = '40' }: ProgressCircleProps) => {
     const { palette } = useTheme();
     const colors = tokens(palette.mode);
-    const angle = parseInt(progress) * 360;
+    const angle = String(Number(progress) * 360);
 
     return (
         <>
             <Box
                 sx={{
                     background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
-                conic-gradient(transparent 0%, ${angle}deg ${colors.blueAccent[500]}
-                ${angle}deg 360deg),
-                ${colors.greenAccent[500]}`,
-                    borderRadius: '50%',
+            conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
+            ${colors.greenAccent[500]}`,
+                    borderRadius: "50%",
                     width: `${size}px`,
                     height: `${size}px`,
                 }}
             />
-
         </>
     )
 }
