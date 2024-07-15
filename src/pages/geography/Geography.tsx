30bfea61ebcc4fb9541f4d0d8f4d2282247a7ea0
Murod Akhmedov <murod.london@gmail.com>
2024-07-15 14:58:14 +0100

    Create geographyChart  component

--- src/pages/geography/Geography.tsx
+++ src/pages/geography/Geography.tsx
@@ -1,7 +1,28 @@
+import { Box, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import GeographyChart from "../../components/GeographyChart";
+import { tokens } from "../../theme";
+
 
 const Geography = () => {
+    const { palette } = useTheme();
+    const colors = tokens(palette.mode);
+
     return (
-        <div>Geography</div>
+        <>
+            <Box m="20px">
+                <Header title="Geography" subtitle="Find where your users are located." />
+                <Box
+                    height="75vh"
+                    border={`1px solid ${colors.grey[100]}`}
+                    borderRadius="4px"
+                    mt="10px"
+                    p="10px"
+                >
+                    <GeographyChart />
+                </Box>
+            </Box>
+        </>
     )
 }
 
